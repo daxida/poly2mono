258e0302730e6b7f83eae30f4f677ace0c89592d
daxida <galmaw@hotmail.com>
2024-11-11 08:35:18 +0100

    Move test

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
+import re
 import unittest
 from functools import partialmethod
 from typing import Iterator
 
 from poly2mono import poly2mono
+from utils.remove_spirits import create_dictionary_spirits
 
 Pair = Iterator[tuple[str, str]]
 Pairs = list[Pair]
@@ -43,6 +45,42 @@ class TestPoly(unittest.TestCase):
     # This makes so each pair counts as a separate test
     for n, test in enumerate(tests_pairs):
         locals()[f"test_{n}"] = partialmethod(test_builder, test)
+
+    def test_remove_spirits(self):
+        text = (
+            "Οπὼς, ᾘσθάνθην, Ἐλπίζουμε ἡ ἱστοσελίδα αὐτὴ νὰ πείσει ὅσους ἐπισκέπτες "
+            "θεωροῦν τὸ πολυτονικὸ ξεπερασμένο καὶ ἄχρηστο ὅτι συμβαίνει ἀκριβῶς τὸ "
+            "ἀντίθετο: οἱ τόνοι καὶ τὰ πνεύματα εἶναι ἀναπόσπαστο μέρος τῆς ἑλληνικῆς "
+            "γλώσσας· θεωροῦμε δὲ ὅτι ἡ «μονοτονικὴ μεταρρύθμιση» τοῦ 1982 ἦταν ἕνα "
+            "τραγικὸ λάθος εἰς βάρος τοῦ ἑλληνικοῦ πολιτισμοῦ. Καὶ τὸ χειρότερο: ἡ "
+            "μονοτονικὴ γραφὴ ἐπιβλήθηκε τότε ἀπὸ τὴν κρατικὴ ἐξουσία στὰ σχολεῖα "
+            "(συχνὰ μὲ τὴν συνένοχη συμμετοχὴ ἀνενημέρωτων ἢ ἀνευθύνων δασκάλων καὶ "
+            "καθηγητῶν φιλολόγων) μὲ ἀποτέλεσμα οἱ σημερινοὶ νέοι νὰ μεγάλωσαν μὲ τὴν "
+            "πεποίθηση ὅτι οἱ τόνοι καὶ τὰ πνεύματα εἶναι ἄχρηστοι καὶ ἀνήκουν στὸ "
+            "παρελθόν. Εἶναι χρέος μας νὰ ἐπανορθώσουμε αὐτὸ τὸ λάθος."
+        )
+        text_expected = (
+            "Οπώς, Ησθάνθην, Ελπίζουμε η ιστοσελίδα αυτή νά πείσει όσους επισκέπτες "
+            "θεωρούν τό πολυτονικό ξεπερασμένο καί άχρηστο ότι συμβαίνει ακριβώς τό "
+            "αντίθετο: οι τόνοι καί τά πνεύματα είναι αναπόσπαστο μέρος τής ελληνικής "
+            "γλώσσας· θεωρούμε δέ ότι η «μονοτονική μεταρρύθμιση» τού 1982 ήταν ένα "
+            "τραγικό λάθος εις βάρος τού ελληνικού πολιτισμού. Καί τό χειρότερο: η "
+            "μονοτονική γραφή επιβλήθηκε τότε από τήν κρατική εξουσία στά σχολεία "
+            "(συχνά μέ τήν συνένοχη συμμετοχή ανενημέρωτων ή ανευθύνων δασκάλων καί "
+            "καθηγητών φιλολόγων) μέ αποτέλεσμα οι σημερινοί νέοι νά μεγάλωσαν μέ "
+            "τήν πεποίθηση ότι οι τόνοι καί τά πνεύματα είναι άχρηστοι καί ανήκουν στό "
+            "παρελθόν. Είναι χρέος μας νά επανορθώσουμε αυτό τό λάθος."
+        )
+
+        def translate(dictionary, text):
+            for k, v in dictionary.items():
+                text = re.sub(k, v, text)
+            return text
+
+        dictionary = create_dictionary_spirits()
+        for word, expected in zip(text.split(), text_expected.split()):
+            received = translate(dictionary, word)
+            assert received == expected, f"word {word}, expected {expected}"
 
 
 if __name__ == "__main__":
